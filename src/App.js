35ce811bfd9f2815a6f0124dc9895283de3365e6
leonardtan13 <leonardtan13@gmail.com>
2021-05-13 20:12:53 +0800

    added globalstyles

--- src/App.js
+++ src/App.js
@@ -1,10 +1,9 @@
-import React from "react";
-
+import TopBar from "./components/TopBar/TopBar"
 
 function App() {
   return (
     <div className="App">
-
+      <TopBar />
     </div>
   );
 }
--- src/GlobalStyles.js
+++ src/GlobalStyles.js
@@ -1,3 +1,7 @@
+import styled, {createGlobalStyle} from 'styled-components';
+
+const GlobalStyles = createGlobalStyle`
+
 * {
   margin: 0;
   padding: 0;
@@ -18,4 +22,31 @@ code {
   font-family: source-code-pro, Menlo, Monaco, Consolas, 'Courier New',
     monospace;
 }
+`;
 
+export default GlobalStyles;
+
+export const Container = styled.div`
+margin: 0 auto;
+padding: 0 50px;
+max-width: 1300px;
+width: 100%;
+
+@media (max-width:400px){
+    padding: 0 10px;
+}
+@media (max-width:991px) {
+    padding: 0 30px;
+}
+
+@media (min-width: 1500px) {
+    max-width: 1500px;
+}
+
+@media (min-width: 1800px) {
+    max-width: 1800px;
+    padding: 0 30px;
+}
+`;
+
+<GlobalStyles/>
